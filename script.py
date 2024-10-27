ee3d95496fe0b5c3e518362f5ba6fd7443d4154b
Wheelspawn <nsage@protonmail.com>
2024-10-27 17:20:46 -0500

    error handling

--- script.py
+++ script.py
@@ -3,18 +3,20 @@ import time
 import os
 from groq import Groq
 
+import streamlit as st
+st.title('Autonomous DJ')
+
 import base64
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-
 def main():
     
     # Path to your image
 
-    image_path = "/home/nsage/Pictures/napoleon.jpg"
+    image_path = "/home/nsage/Pictures/empty.jpeg"
     # Getting the base64 string
     base64_image = encode_image(image_path)
     
@@ -23,40 +25,45 @@ def main():
     )
     
     while True:
-    
-        completion = client.chat.completions.create(
-            model="llama-3.2-11b-vision-preview",
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": "If the people in this photo are standing or dancing, output 'dancing'. If they are sitting, output 'sitting'. If there are no people in the photo, output 'empty'. You are outputting a single keyword. Do not output anything other than one of these three words. Do not output a sentence or paragraph. Do not output any punctuation."
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": f"data:image/jpeg;base64,{base64_image}",
-                            }
-                        }
-                    ]
-                },
-                {
-                    "role": "assistant",
-                    "content": ""
-                }
-            ],
-            temperature=1,
-            max_tokens=1024,
-            top_p=1,
-            stream=False,
-            stop=None,
-        )
-    
-        print(completion.choices[0].message)
         
-        time.sleep(0.5)
+        try:
+    
+            completion = client.chat.completions.create(
+                model="llama-3.2-90b-vision-preview",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": "If the people in this photo are standing or dancing, output 'dancing'. If they are sitting, output 'sitting'. If there are no people in the photo, or there is no photo, output 'empty'. You are outputting a single keyword. Do not output anything other than one of these three words. Do not output a sentence or paragraph. Do not output any punctuation."
+                            },
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/jpeg;base64,{base64_image}",
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "role": "assistant",
+                        "content": ""
+                    }
+                ],
+                temperature=1,
+                max_tokens=1024,
+                top_p=1,
+                stream=False,
+                stop=None,
+            )
+    
+            print(completion.choices[0].message)
+        
+            time.sleep(1)
+            
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
